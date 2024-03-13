1fc48fcd2551e896e61e5ccab7a4f25182ffed03
Fen1x <baurasm@bk.ru>
2024-03-13 08:50:13 +0300

    исправления мейн

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -9,6 +9,11 @@ class ImagesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        
+        tableView.register(
+            ImagesListCell.self,
+            forCellReuseIdentifier: ImagesListCell.reuseIdentifier
+        )
     }
 
     private lazy var dateFormatter: DateFormatter = {
